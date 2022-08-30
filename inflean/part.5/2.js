4f1cef378d5f319fbd08b051d37c75bc66efd017
rondido <rppr001@naver.com>
2022-08-31 00:01:57 +0900

    part.5 two pointers algorithm two commit

--- inflean/part.5/2.js
+++ inflean/part.5/2.js
@@ -1,19 +1,23 @@
 //공통원소 구하기
 function solution(arr1, arr2) {
-    let answer = [];
-    arr1.sort((a,b)=> a-b);
-    arr2.sort((a,b)=> a-b);
-    let m = arr1.length;
-    let n = arr2.length;    
-    let p1=p2=0;
-    while(p1 < m && p2 <n){
-        if(arr1[p1] === arr2[p2]) {
-            answer.push(arr1[p1++]);            
-            p2++;
-        }else if(arr1[p1]< arr2[p2]) p1++;
-        else p2++;
-    }
-    return answer;
+  let answer = [];
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+  let p1 = 0;
+  let p2 = 0;
+  let n = arr1.length;
+  let m = arr2.length;
+  //p1의 값이 arr1.length의 길이를 넘어버리면 거짓이므로 while에서 나옴
+  //p2의 값이 arr2.length의 길이를 넘어버리면 거짓이므로 while에서 나옴
+  while (p1 < n && p2 < m) {
+    if (arr1[p1] === arr2[p2]) {
+      answer.push(arr1[p1++]);
+      p2++;
+    } else if (arr1[p1] < arr2[p2]) p1++;
+    else p2++;
+  }
+
+  return answer;
 }
 
 let a = [1, 3, 9, 5, 2];
