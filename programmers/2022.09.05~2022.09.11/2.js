944c071fd6c4f7611d7737af8284a060b312a392
rondido <rppr001@naver.com>
2022-09-07 21:53:41 +0900

    programmers study week1 commit

---
+++ programmers/2022.09.05~2022.09.11/2.js
@@ -0,0 +1,12 @@
+//lv1
+//약수의 합 구하기
+function solution(n) {
+  var answer = 0;
+  let moc = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      answer += i;
+    }
+  }
+  return answer;
+}
