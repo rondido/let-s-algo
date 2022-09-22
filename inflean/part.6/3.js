755b90c3b5421768f541744f3166e79a4bf678e3
rondido <rppr001@naver.com>
2022-09-22 18:36:47 +0900

    inflean algorithm commit

--- inflean/part.6/3.js
+++ inflean/part.6/3.js
@@ -1,10 +1,25 @@
 //크리인 인형뽑기
 function solution(board, moves) {
   let answer = 0;
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board.length; j++) {}
-  }
+  let stack = [];
 
+  moves.forEach((pos) => {
+    for (let i = 0; i < board.length; i++) {
+      //인형뽑기에서 인형을 뽑는다.
+      if (board[i][pos - 1] !== 0) {
+        //인형뽑기에서 뽑아온 인형을 tmp에 넣어주고
+        //인형뽑기 안에는 뽑아온 인형의 위치를 0으로 초기화 시킨다.
+        let tmp = board[i][pos - 1];
+        board[i][pos - 1] = 0;
+        //인형뽑기에서 뽑아온 값과 stack 바구니에 제일 끝에 있는 값이 같으면 터트린다.
+        if (tmp == stack[stack.length - 1]) {
+          stack.pop();
+          answer += 2;
+        } else stack.push(tmp);
+        break;
+      }
+    }
+  });
   return answer;
 }
 
