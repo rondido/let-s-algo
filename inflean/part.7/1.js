b960600bdd18a2a73c4a8d5fa8a4ecd728027e89
rondido <rppr001@naver.com>
2022-11-22 23:58:39 +0900

    선택정렬 commit

---
+++ inflean/part.7/1.js
@@ -0,0 +1,18 @@
+//선택 정렬
+function solution(arr) {
+  let answer = arr;
+  let abc;
+  for (let i = 0; i < arr.length - 1; i++) {
+    let mainindex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[mainindex] > arr[j]) {
+        mainindex = j;
+      }
+      [arr[i], arr[mainindex]] = [arr[mainindex], arr[i]];
+    }
+  }
+  return answer;
+}
+
+let arr = [13, 5, 11, 7, 23, 15];
+console.log(solution(arr));
