94054bde3302965e474360c2c1382d03639f36dc
rondido <rppr001@naver.com>
2023-05-24 01:38:15 +0900

    queue 직접 구현 예시 코드 작성

--- baekjoon/자료구조/큐/18258.js
+++ baekjoon/자료구조/큐/18258.js
@@ -1,27 +1,85 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+const [n, ...arr] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .split("\n");
 
-const num = Number(input[0]);
-
-const que = new Queue();
-const answer = [];
-
-for (let i = 1; i <= num; i++) {
-  let result = input[i].split(" ");
-  switch (result[0]) {
-    case "push":
-      que.push(result[1]);
-      break;
-    case "pop":
-      answer.push(que.shift() || -1);
-    case "size":
-      answer.push(que.length);
-    case "empty":
-      answer.push(que.length === 0 ? 1 : 0);
-    case "front":
-      answer.push(que[0] || -1);
-    case "back":
-      answer.push(que[que.length - 1] || -1);
+class Node {
+  constructor(input) {
+    this.input = input;
+    this.next = null;
   }
 }
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(input) {
+    const node = new Node(input);
+    if (this.head === null) {
+      this.head = node;
+      this.head.next = null;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+    this.length += 1;
+  }
+  pop() {
+    if (this.empty() === 1) return -1;
+    const popItem = this.head;
+    this.head = this.head.next;
+    this.length -= 1;
+    return popItem.input;
+  }
+  size() {
+    return this.length;
+  }
+  empty() {
+    if (this.length == 0) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+  front() {
+    if (this.empty() == 1) return -1;
+    return this.head.input;
+  }
+  back() {
+    if (this.empty() == 1) return -1;
+    return this.tail.input;
+  }
+}
+
+let answer = [];
+let queue = new Queue();
+
+const command = arr.map((v) => v.split(" "));
+
+command.forEach((v) => {
+  switch (v[0]) {
+    case "push":
+      queue.push(v[1]);
+      break;
+    case "pop":
+      answer.push(queue.pop());
+      break;
+    case "size":
+      answer.push(queue.size());
+      break;
+    case "empty":
+      answer.push(queue.empty());
+      break;
+    case "front":
+      answer.push(queue.front());
+      break;
+    case "back":
+      answer.push(queue.back());
+      break;
+  }
+});
 
 console.log(answer.join("\n"));
--- baekjoon/자료구조/큐/README.md
+++ baekjoon/자료구조/큐/README.md
@@ -1,1 +1,7 @@
 https://www.acmicpc.net/submit/18258/61205565
+
+Javascript에서 queue는 존재하지않는다.
+
+queue를 비슷하게 구현하기 위해 배열로 push,shift를 사용하여 구현할 수 있지만 shift의 시간복잡도는 o(n)이다.
+
+그렇기 때문에 queue 구현하여야 백준에서 시간초과를 에러를 방지할 수 있다.
