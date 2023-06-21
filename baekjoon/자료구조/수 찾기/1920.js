48df1cba2456c2247aaa7724e1caf9ed7f8b3bbc
rondido <rppr001@naver.com>
2023-06-21 18:21:36 +0900

    자료구조  수 찾기

---
+++ baekjoon/자료구조/수 찾기/1920.js
@@ -0,0 +1,39 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, A, M, B] = input.map((v) => v.split(" ").map((x) => Number(x)));
+
+const arr = new Set(A);
+
+let result = B.map((v) => (arr.has(v) ? 1 : 0));
+console.log(result.join("\n"));
+
+// 이분탐색
+
+const [N, A, M, B] = input.map((v) => v.split(" ").map((x) => Number(x)));
+
+// A.sort((a, b) => a - b);
+
+// // 이분 탐색
+// const binarySearch = (list, target, left, right, mid) => {
+//   mid = Math.floor((left + right) / 2);
+
+//   if (right < left) {
+//     return list[mid] == target ? 1 : 0;
+//   }
+
+//   if (list[mid] > target) {
+//     right = mid - 1;
+//   } else {
+//     left = mid + 1;
+//   }
+
+//   return binarySearch(list, target, left, right, mid);
+// }
+
+// const result = B.map(v => binarySearch(A, v, 0, A.length - 1, 0));
+
+// console.log(result.join("\n"));
