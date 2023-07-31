4e7e69ef8fed7fa347dae1443cd35045b18d6b1c
rondido <rppr001@naver.com>
2023-07-31 21:30:40 +0900

    자료구조 스택(코딩테스트 스터디로 인한 재 풀이)

--- baekjoon/자료구조/스택/10828.js
+++ baekjoon/자료구조/스택/10828.js
@@ -1,31 +1,28 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-const num = Number(input[0]);
+const len = input.shift();
 
-let stack = [];
-let answer = [];
-for (let i = 0; i <= num; i++) {
-  let result = input[i].split(" ");
+const stack = [];
+const answer = [];
+
+for (let i = 0; i < len; i++) {
+  const result = input[i].split(" ");
   switch (result[0]) {
     case "push":
       stack.push(result[1]);
       break;
-    case "pop":
-      answer.push(stack.pop() || -1);
-      break;
     case "size":
       answer.push(stack.length);
-
-      break;
-    case "empty":
-      answer.push(stack[0] ? 0 : 1);
-
       break;
     case "top":
       answer.push(stack[stack.length - 1] || -1);
-
+      break;
+    case "pop":
+      answer.push(stack.pop() || -1);
+      break;
+    case "empty":
+      answer.push(stack[0] ? 0 : 1);
       break;
   }
 }
-
 console.log(answer.join("\n"));
