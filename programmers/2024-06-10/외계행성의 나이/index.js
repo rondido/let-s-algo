27ad34f803442da0b9b95631acaec1a479543407
rondido <rppr001@naver.com>
2024-06-10 23:40:10 +0900

    프로그래머스 입문 문제

---
+++ programmers/2024-06-10/외계행성의 나이/index.js
@@ -0,0 +1,9 @@
+function solution(age) {
+  let alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+  var answer = "";
+  age = age.toString();
+  for (let i = 0; i < age.length; i++) {
+    answer += alpha[age[i]];
+  }
+  return answer;
+}
