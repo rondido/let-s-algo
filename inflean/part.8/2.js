de367268f5a866a6619668e2c47ff0843dc213e8
rondido <rppr001@naver.com>
2024-07-23 00:08:28 +0900

    dfs: 이진수 출력(dfs)

---
+++ inflean/part.8/2.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  let answer = "";
+  function DFS(L) {
+    if (L === 0) return;
+    else {
+      DFS(parseInt(L / 2));
+      answer += L % 2;
+    }
+  }
+  DFS(n);
+  return answer;
+}
+console.log(solution(11));
