81cdb0ad6b487a5a9091c6341f0aa02083b8b7ff
rondido <rppr001@naver.com>
2024-12-24 12:01:11 +0900

    이진탐색 메타트로닉서치 예산 문제 풀이

---
+++ fastcampus/ch5/binarysearch/2512.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./2512.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+let n = Number(input[0].split(" ")[0]); //지방의 수N
+let arr = input[1].split(" ").map(Number); //각 지방의 예산 요청
+let m = Number(input[2]); //총 예산
+
+let start = 1;
+let end = arr.reduce((a, b) => Math.max(a, b));
+
+let result = 0;
+while (start <= end) {
+  let mid = parseInt((start + end) / 2);
+  let total = 0;
+  for (x of arr) {
+    total += Math.min(mid, x);
+  }
+  if (total <= m) {
+    result = mid;
+    start = mid + 1;
+  } else {
+    end = mid - 1;
+  }
+}
+
+console.log(result);
